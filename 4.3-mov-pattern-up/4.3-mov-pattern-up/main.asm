21ed79174aa724a2f52ace909548d36bb2adfe2b
Michal Durinik <michal.durinik2@mail.dcu.ie>
2017-07-06 19:56:28 +0100

    done + extra comments

--- 4.3-mov-pattern-up/4.3-mov-pattern-up/main.asm
+++ 4.3-mov-pattern-up/4.3-mov-pattern-up/main.asm
@@ -23,36 +23,36 @@ count			DWORD	32
 r_count			DWORD	0
 row0			DWORD	0
 row1			DWORD	1
-msg				BYTE	"Please input pattern", 0, 10
+in_msg			BYTE	"Please input pattern", 0, 10
 
 .code
 	main:nop
 
-		invoke readIntegerWithMessage, addr msg
-		invoke setPattern, eax
+		invoke readIntegerWithMessage, addr in_msg		;user inser input, integer is stored in eax register
+		invoke setPattern, eax							;set LED pattern from register eax
 		
-l1:		mov row0, 0
-		mov row1, 1
-		mov r_count, 0
+l1:		mov row0, 0						;row0 = 0, (resetting rows and row count to 0)
+		mov row1, 1						;row1 = 1, (after whole cycle finishes)
+		mov r_count, 0					;r_count = 0, (of patern moving up by 1)
 
-		cmp count, 0
-		jl finish
-
-		mov eax, count
-l2:		cmp r_count, eax
-		je l3
-
-		invoke readRow, row1
-		invoke writeRow, row0, eax
-		inc row0
+		cmp count, 0					;if count < 0, (when loop repeats 32times, program finish)
+		jl finish						;then finish, (jump to finish)
+										;else,
+		mov eax, count					;eax = count (moving overall counter to register for comparation)
+l2:		cmp r_count, eax				;if r_count == eax, (comparing total count with rowcounter)
+		je l3							;then,  (jump to l3)
+										;else
+		invoke readRow, row1			;eax = row1, (read LED row on position row1, store in register eax)
+		invoke writeRow, row0, eax		;row0 = eax, (write eax LED row to row0)
+		inc row0						;row0 += 1, (incrementing rows positions and row counter in next 2 lines)
 		inc row1
 		inc r_count
-		mov eax, count
-		jmp l2
+		mov eax, count					;eax = count, (moving overall counter to register for comparation)
+		jmp l2							;jump to l2, (l2 is inner loop, moving every line 1by1)
 
-l3:		dec count
-		invoke Sleep, 500
-		jmp l1
+l3:		dec count						;count -= 1, (after moving entire LED field by 1, decrease main counter)
+		invoke Sleep, 500				;sleep for 500ms for people to observe change
+		jmp l1							;jump to l1, to start another cycle, (l1 is outside or main, loop)
 
 finish:
 		invoke ExitProcess,0
