1b1ab87ffe032a1e787bb1da142627b59513adef
MichalDurinik <michal.durinik2@mail.dcu.ie>
2017-07-07 23:13:52 +0100

    starting

---
+++ Lab-ex-AA-4.11.16/Lab-ex-AA-4.11.16/main.asm
@@ -0,0 +1,30 @@
+
+.586
+.model flat,stdcall
+.stack 4096
+option casemap:none
+
+include     p:\masm32\include\windows.inc
+include     p:\masm32\include\kernel32.inc
+include		p:\masm32\include\user32.inc
+include		p:\masm32\include\msvcrt.inc
+include		p:\masm32\include\ca296.inc
+
+includelib p:\masm32\lib\ca296.lib
+includelib kernel32.lib
+includelib user32.lib
+includelib msvcrt.lib
+
+
+.data
+
+
+.code
+	main:nop
+
+
+
+
+		invoke ExitProcess,0
+
+	end main
