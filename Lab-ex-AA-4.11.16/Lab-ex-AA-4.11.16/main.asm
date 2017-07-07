a8e776ddb593775955e666dd3cccaa953d71a488
Michal Durinik <michal.durinik2@mail.dcu.ie>
2017-07-07 23:58:26 +0100

    done

--- Lab-ex-AA-4.11.16/Lab-ex-AA-4.11.16/main.asm
+++ Lab-ex-AA-4.11.16/Lab-ex-AA-4.11.16/main.asm
@@ -1,3 +1,4 @@
+;choose row, then switch the leds off from left to right, 1by1, 5-9sec to finish.
 
 .586
 .model flat,stdcall
@@ -17,14 +18,32 @@ includelib msvcrt.lib
 
 
 .data
-
+in_msg			BYTE	"Please input row to clear", 0, 10
+count			DWORD	31
 
 .code
 	main:nop
 
+		invoke readIntegerWithMessage, addr in_msg
+		mov ebx, eax
+		invoke setPattern, 1
 
+l1:		invoke readRow, ebx				;row number in ebx
+		cmp count, 0
+		jl finish
 
+		mov ecx, count					;starting from position coord, x=31, ie. count value
+		mov edx, 1						;1 in edx
+		shl edx, CL						;move this 1 to position 31, 1000000....
+		not edx							;flip binary, 0111111111.....
+		and eax, edx					;where is 0and0 = 0, 0and1 = 0 also
 
+		invoke writeRow, ebx, eax		;store modified row back.
+		invoke Sleep, 200
+		dec count
+		jmp l1
+
+finish:
 		invoke ExitProcess,0
 
 	end main
